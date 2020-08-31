7e15d38c01e1daf1ee120b41ab55a0d8ff2b75c4
黄竹斌 <zbhuang@travelsky.com>
2020-08-31 22:12:55 +0800

    bus服务端刷新客户端刷新

---
+++ cloud-config-client-3366/src/main/java/com/zbhuang/springcloud/ConfigMain3366.java
@@ -0,0 +1,13 @@
+package com.zbhuang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigMain3366.class,args);
+    }
+}
